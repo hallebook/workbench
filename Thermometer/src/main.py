85a26d5b1e2c496177953d88ed23675a05afe1ca
hallebook <book01@allegheny.edu>
2023-09-11 09:51:10 -0400

    temperature coding

--- Thermometer/src/main.py
+++ Thermometer/src/main.py
@@ -13,13 +13,18 @@ Expression course.
 
 def main():
     
-    # TODO: Print program name
+    # Print program name
+    print("Farenheight to celsius converter", end = "\n\n")
 
-    # TODO: Request user input
+    # Request user input
+    temp_f = input("Enter the temperature (without units): ")
+    print(temp_f)
 
-    # TODO: Perform calculation
+    # Perform calculation
+    temp_c = (5/9) * (int(temp_f) - 32)
 
-    # TODO: Report result
+    # Report result
+    print("The temperature in Celsius is: ", temp_c)
 
 if __name__ == "__main__":
     main()
